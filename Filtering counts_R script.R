b730a82af7d0f13c83e04482d5c3e4943c3dbb83
Donovin Coles <148016148+Coles-DW@users.noreply.github.com>
2025-02-01 13:29:05 +1100

    Add files via upload
    This script may be used to filter gene expression count data from your microbe of interest. The output is a file that only contains row, i.e. genes that have at least 10 counts in at least one sample, i.e. the column.

---
+++ Filtering counts_R script.R
@@ -0,0 +1,24 @@
+setwd("")
+install_course_packages.R
+install.packages("usethis")
+expdata <- read.csv("Unique counts_all samples_05102020.csv")
+str(expdata)
+
+mat <- as.matrix(expdata[, -1])
+rownames(mat) <- expdata[, 1]
+str(mat)
+
+# zeros any cells (gene in a sample) where read count is less than a threshold, here 10
+# then convert cells above that threshold to '1', resulting in a binary matrix
+mat[mat < 10] <- 0
+mat[mat > 0] <- 1
+
+# the number of genes that have expression above the threshold in at least X replicates
+sum(rowSums(mat) >= 1)
+sum(rowSums(mat) >= 3)
+
+# remove any genes that do not meet the threshold in at least X replicates
+expdata.fil <- expdata[rowSums(mat) >= 1, ]
+
+write.csv(expdata.fil, 'filtered_unique_counts.csv', row.names=F)
+write.table(expdata.fil, 'filteredcounts1.csv', row.names=F, sep=';')
